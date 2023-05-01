ad637a3b82f90dff0508922458bf5983cb11b9fa
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-04-30 22:59:35 -0700

    Removed Prof's code

--- App.ts
+++ App.ts
@@ -175,54 +175,6 @@ class App {
 
     // DELETE Routes
 
-    // TODO: delete this
-    // router.get('/app/list/:listId/count', (req, res) => {
-    //     var id = req.params.listId;
-    //     console.log('Query single list with id: ' + id);
-    //     this.Tasks.retrieveTasksCount(res, {listId: id});
-    // });
-
-    // router.post('/app/list/', (req, res) => {
-    //   const id = crypto.randomBytes(16).toString("hex");
-    //   console.log(req.body);
-    //     var jsonObj = req.body;
-    //     jsonObj.listId = id;
-    //     this.Lists.model.create([jsonObj], (err) => {
-    //         if (err) {
-    //             console.log('object creation failed');
-    //         }
-    //     });
-    //     res.send('{"id":"' + id + '"}');
-    // });
-
-    // router.post('/app/list2/', (req, res) => {
-    //   const id = crypto.randomBytes(16).toString("hex");
-    //   console.log(req.body);
-    //     var jsonObj = req.body;
-    //     jsonObj.listId = id;
-    //     let doc = new this.Lists.model(jsonObj);
-    //     doc.save((err) => {
-    //        console.log('object creation failed');
-    //     });
-    //     res.send('{"id":"' + id + '"}');
-    // });
-
-    // router.get('/app/list/:listId', (req, res) => {
-    //     var id = req.params.listId;
-    //     console.log('Query single list with id: ' + id);
-    //     this.Tasks.retrieveTasksDetails(res, {listId: id});
-    // });
-
-    // router.get('/app/list/', (req, res) => {
-    //     console.log('Query All list');
-    //     this.Lists.retrieveAllLists(res);
-    // });
-
-    // router.get('/app/listcount', (req, res) => {
-    //   console.log('Query the number of list elements in db');
-    //   this.Lists.retrieveListCount(res);
-    // });
-
     this.expressApp.use('/', router);
 
     this.expressApp.use('/app/json/', express.static(__dirname + '/app/json'));
