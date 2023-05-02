c183e779fead91a597dfcb5b60e0ecf66cb6e246
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-01 22:57:22 -0700

    Updated routes

--- App.ts
+++ App.ts
@@ -63,26 +63,52 @@ class App {
     /* Item Routes */
 
     /* GET Routes */
+
+    /**
+     * Get all items
+     * @returns - JSON obj of all items as a response
+     */
     router.get('/api/items/', (req, res) => {
       console.log('Query All items');
       this.Items.retrieveAllItems(res);
     });
+
+    /**
+     * Get a specific item by ID
+     * @param itemID
+     *  - The ID of the item to retrieve
+     * @returns
+     * - JSON obj of the item with the specified ID as a response
+     */
     router.get('/api/getItem/:itemID', (req, res) => {
       console.log('Query item with itemID');
       let filter: object = {
         itemID: req.params.itemID
       };
-      this.Items.getItem(res,filter);
+      res.json(this.Items.getItem(filter));
     });
-     /* POST Routes */
+
+
+    /* POST Routes */
+
+    /**
+     * Create a new item
+     * @param req
+     *  - itemName: string - name of the item
+     * - itemDescription: string - description of the item
+     * - itemPrice: number - price of the item
+     * - itemImg: string - URL of the image of the item
+     * - restaurantID: string - ID of the restaurant to which the item is associated
+     * - menusID: string - ID of the menu to which the item is associated
+     */
     router.post('/api/createItem', (req, res) => {
       console.log('Insert item into items collection');
       let createItem: object = {
-        itemName:req.body.itemName,
+        ItemID: crypto.randomUUID(),
+        itemName: req.body.itemName,
         itemDecription:req.body.itemDecription,
         itemPrice:req.body.itemPrice,
-        itemImg : req.body.itemImg,
-        ItemID: crypto.randomUUID(),
+        itemImg: req.body.itemImg,
         restaurantID: req.body.restaurantId,
         menusID:req.body.menusid
        
@@ -97,7 +123,12 @@ class App {
 
     /* GET Routes */
 
-    // Get all menus
+    /**
+     * Get all menus
+     * @param req
+     * - restaurantId: string - ID of the restaurant for which to retrieve all menus
+     * @returns - JSON obj of all menus as a response
+     */
     router.get('/api/menus/:restaurantId', (req, res) => {
       // Get the RestaurantId URL parameters
       let restaurantId: string = req.params.restaurantId;
@@ -108,7 +139,11 @@ class App {
       this.Menus.retrieveAllMenus(res, { restaurantID: restaurantId });
     });
 
-    // Get menu sections
+    /**
+     * Get all menu sections for a specific menu for a specific restaurant
+     * @param restaurantId: string - ID of the restaurant for which to retrieve all menus
+     * @param menuId: string - ID of the menu for which to retrieve all menu sections
+     */
     router.get('/api/menus/:restaurantId/:menuId', (req, res) => {
       // Get the RestaurantId, RestaurantOwnerId, and menuId from URL parameters
       let restaurantId: string = req.params.restaurantId;
@@ -153,8 +188,14 @@ class App {
     });
 
 
-
-    // Add a section to a menu
+    /**
+     * Add a new section to an existing menu
+     * @param req
+     * - restaurantId: string - restaurantID to which the menu belongs
+     * - menuId: string - menuID to which the new section is to be added
+     * - sectionName: string - name of the section to be added
+     * @param res json obejct of the new menu section
+     */
     router.post('/api/menus/add-section', (req, res) => {
       // Get the RestaurantId, menuId, sectionName from req body
       let restaurantId: string = req.body.restaurantId;
@@ -167,43 +208,64 @@ class App {
       this.Menus.addMenuSection(res, { restaurantID: restaurantId, menuID: menuId }, sectionName);
     });
 
-    // Add item to a section in menu
+    /**
+     * Add an existing item to an existing section in an existing menu
+     * @param req
+     *  - restaurantId: string - restaurantID to which the menu belongs
+     *  - menuId: string - menuID to which the section belongs
+     *  - menuSection: string - menuSection to which the item  should be added to
+     *  - itemId: string - itemID of the item to be added
+     */
     router.post('/api/items/add-item', (req, res) => {
       // Get the RestaurantId, menuId, sectionName, itemId from req body
       let restaurantID: string = req.body.restaurantID;
       let menuID: string = req.body.menuID;
-      let menuSection: string = req.body.menuSection;
+      let sectionName: string = req.body.menuSection;
       let itemID: string = req.body.itemID;
 
       try{
         // Pre check: check if the item exists in the database
-        // this.Items.retrieveItemByID(res, { itemId: itemId }); // TODO : implement this
-        // console.log("Adding " + itemName + " : " + menuId + " for restaurant: " + restaurantId);
-        // Query the database to add a section to the menu
+        const checkItem = this.Items.getItem({ itemID: itemID });
+        // check for empty JSON
+        if (JSON.stringify(checkItem) === '{}') {
+          res.status(400).json({ message: 'Item is not found' });
+          return;
+        }
 
-        const item = this.Items.checkItem({ itemID: itemID });
-        if (item == false) {
-        res.status(400).json({ message: 'Item is not found' });
-        }
-        // check again if the menu item is in the menu section
-        const existingItem = this.Menus.checkItemInSection({restaurantID: restaurantID, menuID:menuID, menuSection:menuSection}, itemID);
-        //this.menuSection.items.find((itemObj: any) => itemObj.itemId === itemId);
+        // Check if the item already exists in the menu section
+        const existingItem = this.Menus.checkItemInSection({ restaurantID: restaurantID, menuID: menuID }, itemID);
         if (existingItem) {
-        res.status(400).json({ message: 'Item already exists in the section' });
+          res.status(400).json({ message: 'Item already exists in the section' });
+          return;
         }
-        // add the item to the menu section
-        
-       
+
+        // Add the item to the menu section
+        this.Menus.addMenuItem(res,
+          {
+            restaurantID: restaurantID,
+            menuID: menuID,
+            sectionName: sectionName,
+            itemID: itemID
+          }
+        );
         
       } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Internal server error '});
       }
-
     });
 
     // PATCH Routes
 
+    /**
+     * Update the menu time
+     * @param req
+     * - restaurantId: string - restaurantID to which the menu belongs
+     * - menuId: string - menuID whose time is to be updated
+     * - startTime: Date - new start time of the menu
+     * - endTime: Date - new end time of the menu
+     * @param res json obejct of the updated menu
+     */
     router.patch('/api/menus/update-menu-time', (req, res) => {
       // Get the new start and end time from req body
       let startTime: Date = parseTime(req.body.startTime);
