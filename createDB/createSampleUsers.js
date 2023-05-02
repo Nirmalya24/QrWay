86307c4b05bc42a328b5249d6b675182961ddbdc
goyaladit <80494397+goyaladit@users.noreply.github.com>
2023-05-01 21:01:31 -0700

    Added User data and added manager/owner IDs to restaurants

--- createDB/createSampleUsers.js
+++ createDB/createSampleUsers.js
@@ -1,38 +1,57 @@
-/**
- * This script will create sample users for the QRWay application.
- * 
- */
+const MongoClient = require('mongodb').MongoClient;
+const colors = require('colors');
 
-/* Populate All Users */
-db = db.getSiblingDB('qrway_db')
-db.createCollection('Users')
-usersCollection = db.getCollection("Users")
-usersCollection.remove({})
+const url = 'mongodb://qrway_admin:qrway_password@localhost:27017/qrway_db?authMechanism=DEFAULT';
 
-usersCollection.insert(
-    {
-        
+const userData = [
+    {   //restaurant owner
+        userID: "d792c6be-e89c-11ed-a05b-0242ac120003",
+        password: "pwd",
+        connectStatus: false //false by default        
+    },
+    {   //restaurant manager
+        userID: "d792c9a2-e89c-11ed-a05b-0242ac120003",
+        password: "pwd",
+        connectStatus: false //false by default
+    },
+    {   //restaurant manager
+        userID: "d792cb46-e89c-11ed-a05b-0242ac120003",
+        password: "pwd",
+        connectStatus: false //false by default
+    },
+    {   //restaurant manager
+        userID: "d792ccc2-e89c-11ed-a05b-0242ac120003",
+        password: "pwd",
+        connectStatus: false //false by default
+    },
+    {   //restaurant manager
+        userID: "d792d136-e89c-11ed-a05b-0242ac120003",
+        password: "pwd",
+        connectStatus: false //false by default
     }
-)
+    
+];
 
-/* Associate Users to Restaurant Owners */
-db.createCollection('RestaurantOwners')
-ownersCollection = db.getCollection("RestaurantOwners")
-ownersCollection.remove({})
+console.log("[DB Setup] Inserting User data into database...".yellow);
 
-ownersCollection.insert(
-    {
-        
-    }
-)
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(async (client) => {
+        const db = client.db('qrway_db');
 
-/* Associate Users Managers */
-db.createCollection('Managers')
-managersCollection = db.getCollection("Managers")
-managersCollection.remove({})
+        // delete any existing data
+        const deleteResult = await db.collection('Users').deleteMany({});
+        console.log(colors.green('\tDocuments deleted:', deleteResult.deletedCount));
 
-managersCollection.insert(
-    {
-        
-    }
-)
+        // insert data
+        const insertResult = await db.collection('Users').insertMany(userData);
+        console.log(colors.green('\tDocuments inserted:', insertResult.insertedCount));
+
+        // count documents
+        const countResult = await db.collection('Users').countDocuments();
+        console.log(colors.green('\tTotal # of Documents in collection:', countResult));
+
+        await client.close();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
