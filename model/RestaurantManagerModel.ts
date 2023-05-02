3ddb445357eeb4a4b0197e1ade6320eeb502d34a
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-01 23:26:42 -0700

    Ran JS Lint

--- model/RestaurantManagerModel.ts
+++ model/RestaurantManagerModel.ts
@@ -1,71 +1,77 @@
 import Mongoose = require("mongoose");
-import {DataAccess} from '../DataAccess';
-import {IRestaurantManagerModel} from '../interfaces/IRestaurantManagerModel';
+import { DataAccess } from "../DataAccess";
+import { IRestaurantManagerModel } from "../interfaces/IRestaurantManagerModel";
 import { response } from "express";
 
 let mongooseConnection = DataAccess.mongooseConnection;
 let mongooseObj = DataAccess.mongooseInstance;
 
 class RestaurantManagerModel {
-    public schema:any;
-    public model:any;
+    public schema: any;
+    public model: any;
 
-    public constructor () {
+    public constructor() {
         this.createSchema();
         this.createModel();
     }
 
-    public createSchema():void {
-        this.schema = new Mongoose.Schema (
-            {  
+    public createSchema(): void {
+        this.schema = new Mongoose.Schema(
+            {
                 //IUsersModel
-                userID:String,
-                password:String,
-                connectStatus:Boolean,
+                userID: String,
+                password: String,
+                connectStatus: Boolean,
                 //IRestaurantManagerModel
                 managerName: String,
-                restaurantOwnerID:String,
-                restuarantID:String,
-            }, {collection: 'RestaurantManagers'}
+                restaurantOwnerID: String,
+                restuarantID: String,
+            },
+            { collection: "RestaurantManagers" }
         );
     }
 
     public createModel(): void {
-        this.model = mongooseConnection.model<IRestaurantManagerModel>("RestaurantManagers", this.schema);
+        this.model = mongooseConnection.model<IRestaurantManagerModel>(
+            "RestaurantManagers",
+            this.schema
+        );
     }
 
     public error_message(err: any, response: any) {
         if (err) {
             console.log("[RestaurantManager Model] " + err);
-            response.json({ "message": "error", "data": err });
+            response.json({ message: "error", data: err });
         } else {
             console.log("[RestaurantManager Model] Success");
         }
     }
 
-    public retrieveAllRestaurantManagers(res:any): any {
-        console.log("Getting all restaurant managers...");
-        var query = this.model.find({});
-        query.exec( (err, managersArr) => {
+    public retrieveAllRestaurantManagers(
+        res: any,
+        restaurantOwnerID: string
+    ): any {
+        console.log("[RestaurantManager Model] Getting all restaurant managers...");
+        var query = this.model.find({ restaurantOwnerID: restaurantOwnerID });
+        query.exec((err, managersArr) => {
+            if (err) this.error_message(err, res);
             res.json(managersArr);
         });
     }
 
     public createRestaurantManager(res: any, newRestaurantManager: object): any {
-        console.log("Creating restaurant manager...");
-        var query = this.model.create(newRestaurantManager)
+        console.log("[RestaurantManager Model] Creating restaurant manager...");
+        var query = this.model
+            .create(newRestaurantManager)
             .then((restaurantManager) => {
                 res.json(restaurantManager);
             })
             .catch((err) => {
                 this.error_message(err, res);
-            })
+            });
 
         console.log("[RestaurantManager Model] Success!");
     }
-
-
-
 }
 
-export {RestaurantManagerModel}
+export { RestaurantManagerModel };
