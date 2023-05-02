3440c9108323e42d4204e761b90a0b30289deaf8
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-02 14:54:20 -0700

    Update methods to aysnc calls

--- model/RestaurantModel.ts
+++ model/RestaurantModel.ts
@@ -29,13 +29,23 @@ class RestaurantModel {
         this.model = mongooseConnection.model<IRestaurantModel>("Restaurants", this.schema);
     }
 
-    public retrieveAllRestaurants(response:any): any {
-        console.log("start query");
-        var query = this.model.find({});
-        query.exec( (err, itemArray) => {
-            if (err) return console.error(err);
-            response.json(itemArray) ;
-        });
+    /**
+     * Retrieve all restaurants to restaurant owner
+     * 
+     * @param {Object}response - HTTP response Object
+     * @param {string}restaurantOwnerID - Restaurant owner ID
+     * @return {Promise<Array<Object>>} - A Promise to resolve an array of restaurant objects
+     * @throws {err} - throw error when errors occur during databse querying
+     */
+    public async retrieveAllRestaurants(response: any, restaurantOwnerID: string): Promise<any> {
+        try {
+            console.log("start query");
+            const query = this.model.find({ restaurantOwnerID: restaurantOwnerID });
+            const itemArray = await query.exec();
+            return itemArray;
+        } catch (err) {
+            throw err;
+        }
     }
     public retrieveListCount(response:any): any {
         console.log("retrieve Restaurants Count ...");
@@ -46,16 +56,25 @@ class RestaurantModel {
             response.json(numberOfLists) ;
         });
     }
-    public getRestaurantByID(filter: Object): any {
-        // TODO: Implement this
-        // console.log(filter)
-        var query = this.model.findOne(filter);
-        query.exec( (err, itemArray) => {
-            if (err) return console.error(err);
-            //return json object
-            return itemArray;
-        });
 
+    /**
+     * Retrieve restaurant from database that matches the provided filter
+     * 
+     *@param {Object} filter - The filter used to query database
+     *@returns {Promise<Object>} - A Promise resolves to the restaurant document that matches the provided filter
+     *@throws {Error} - throw error when errors occur during databse querying
+     */
+    public async getRestaurantByID(filter: Object): Promise<any> {
+        console.log(`[Menu Model] getRestaurantByID: ${filter['restaurantID']}`);
+        try {
+            const query = this.model.findOne(filter);
+            const itemArray = await query.exec();
+            console.log("[Menu Model | DEBUG] getRestaurantByID: " + itemArray);
+            return itemArray;
+        } catch (err) {
+            throw err;
+        }
     }
+
 }
 export {RestaurantModel};
