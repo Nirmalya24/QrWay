89590409c3ea68c03bdec58611f9209d9ce66f81
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-12 00:21:44 -0700

    Added Restaurant dashboard card component

---
+++ frontend/src/app/dashboard-restaurant-card/dashboard-restaurant-card.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { DashboardServiceService } from '../services/dashboard-service.service';
+
+@Component({
+  selector: 'app-dashboard-restaurant-card',
+  templateUrl: './dashboard-restaurant-card.component.html',
+  styleUrls: ['./dashboard-restaurant-card.component.css']
+})
+export class DashboardRestaurantCardComponent {
+  baseURL: string = 'http://localhost:8000/api';
+  menuID: string = 'b061d548-e85c-11ed-a05b-0242ac120003';
+  menus: any = '';
+
+  constructor(private router: Router, private http: HttpClient, dashboardService: DashboardServiceService) { }
+
+  restaurant: any = {
+    restaurantName: '',
+    restaurantID: '',
+    managerID: [],
+    restaurantOwnerID: '',
+    menusID: []
+  }
+
+  @Input() currentRestaurant = [];
+
+  public viewRestaurant(): any {
+    this.http.get<any>(`${this.baseURL}/menus/${this.menuID}`).subscribe(data => {
+      this.menus = data;
+      console.log(this.menus);
+    })
+
+    //this.router.navigate(['/menus']);
+    console.log('redirect success')
+
+  }
+
+  ngOnInit() {
+    this.viewRestaurant();
+  }
+}
