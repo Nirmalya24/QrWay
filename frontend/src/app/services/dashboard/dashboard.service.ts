e2141ae37c2acac7ac6cc9ee83b20623e64241cc
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-14 12:59:59 -0700

    Renaming dashboard service

---
+++ frontend/src/app/services/dashboard/dashboard.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  private baseURL: string = 'http://localhost:8000/api';
+  private restaurantIDs: any[] = [];
+  private restaurants: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  // Fetch all restaurants from backend
+  public getAllRestaurants(): any {
+    return this.http.get<any>(`${this.baseURL}/restaurant/all/d792c6be-e89c-11ed-a05b-0242ac120003`);
+  }
+}
