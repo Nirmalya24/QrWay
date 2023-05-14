839e901872d2311bac28a1e703db6eb606049d41
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-14 15:57:29 -0700

    Add Dialog logic to layout component

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
+  constructor(public dialog: MatDialog) { }
 
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      data: {
+        title: 'Dialog Title',
+        message: 'This is the dialog message'
+      }
+    });
+  }
 }
