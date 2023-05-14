7143efd0d7ff1ee82cf5afd49be97142393c0845
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-14 12:57:37 -0700

    Add interfaces to Angular project

---
+++ frontend/src/app/share/IMenuModelAngular.ts
@@ -0,0 +1,11 @@
+interface IMenuModelAngular {
+    menuID: string;
+    restaurantID: string;
+    menuName: string;
+    menuItems: [string];
+    menuSections: object;
+    menuDescription: string;
+    menuStartTime: string;
+    menuEndTime: string;
+}
+export default IMenuModelAngular;
