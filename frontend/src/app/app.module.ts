68bfa9e46e654bab79c58810dcad7eee3f1967bb
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-14 15:57:39 -0700

    Update imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -11,6 +10,10 @@ import { RestaurantDashboardComponent } from './restaurant-dashboard/restaurant-
 import { HomeComponent } from './home/home.component';
 import { DashboardRestaurantCardComponent } from './dashboard-restaurant-card/dashboard-restaurant-card.component';
 import { DashboardService } from './services/dashboard/dashboard.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
   declarations: [
@@ -19,13 +22,17 @@ import { DashboardService } from './services/dashboard/dashboard.service';
     LayoutComponent,
     RestaurantDashboardComponent,
     HomeComponent,
-    DashboardRestaurantCardComponent
+    DashboardRestaurantCardComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [DashboardService, HttpClientModule],
   bootstrap: [AppComponent]
