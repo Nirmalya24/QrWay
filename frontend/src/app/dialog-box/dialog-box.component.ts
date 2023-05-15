877e0884aade0c5f9003e308f92d516899895081
goyaladit <80494397+goyaladit@users.noreply.github.com>
2023-05-14 19:59:21 -0700

    added new DialogBoxComponent

---
+++ frontend/src/app/dialog-box/dialog-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-box',
+  templateUrl: './dialog-box.component.html',
+  styleUrls: ['./dialog-box.component.css']
+})
+export class DialogBoxComponent {
+  constructor(public dialogRef: MatDialogRef<DialogBoxComponent>) {}
+}
