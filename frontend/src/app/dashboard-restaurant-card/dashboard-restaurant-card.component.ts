891cbe86aeca15a8159e4f9d0a8a8bb806040982
troy8chen <troy8chen@gmail.com>
2023-05-16 09:48:39 -0700

    Update manage-menu page
    Now we can unload the menu data onto manage-menu page

--- frontend/src/app/dashboard-restaurant-card/dashboard-restaurant-card.component.ts
+++ frontend/src/app/dashboard-restaurant-card/dashboard-restaurant-card.component.ts
@@ -7,7 +7,7 @@ import IRestaurantModelAngular from '../share/IRestaurantModelAngular';
 @Component({
   selector: 'app-dashboard-restaurant-card',
   templateUrl: './dashboard-restaurant-card.component.html',
-  styleUrls: ['./dashboard-restaurant-card.component.css']
+  styleUrls: ['./dashboard-restaurant-card.component.css'],
 })
 export class DashboardRestaurantCardComponent {
   baseURL: string = 'http://localhost:8000/api';
@@ -15,7 +15,7 @@ export class DashboardRestaurantCardComponent {
   menus: any = '';
   
   @Input() currentRestaurant!: IRestaurantModelAngular;
-
+  //restaurantData = this.currentRestaurant.restaurantID;
 
   constructor(private router: Router, private http: HttpClient) {
     this.currentRestaurant = {} as IRestaurantModelAngular
@@ -30,7 +30,7 @@ export class DashboardRestaurantCardComponent {
   }
 
   public viewRestaurant(): any {
-    this.router.navigate(['/menus']);
+    this.router.navigate(['/menus', {"restaurantID":this.currentRestaurant.restaurantID}]);
     console.log('[view button clicked] redirect to', this.currentRestaurant.restaurantID)
 
   }
--- frontend/src/app/manage-menu/manage-menu.component.ts
+++ frontend/src/app/manage-menu/manage-menu.component.ts
@@ -1,31 +1,71 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { DashboardService } from '../services/dashboard/dashboard.service';
 import IRestaurantModelAngular from '../share/IRestaurantModelAngular';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import IMenuModelAngular from '../share/IMenuModelAngular';
+import { JsonPipe } from '@angular/common';
 
 @Component({
-  selector: 'app-manage-menu',
+  selector: 'app-managae-menu',
   templateUrl: './manage-menu.component.html',
-  styleUrls: ['./manage-menu.component.css']
+  styleUrls: ['./manage-menu.component.css'],
+  //template:'<p>{{ menuOutput }}</p>'
 })
 
+
 export class ManageMenuComponent {
-  public menus: any[] = [];
+  //public menus: any[] = [];
 
-  @Input() currentRestaurant!: IRestaurantModelAngular;
+  //@Input() currentRestaurant: IRestaurantModelAngular;
+  menuOutput:any[]=[];
 
-  constructor(private http: HttpClient, private dashboardService: DashboardService) { 
-    this.currentRestaurant = {} as IRestaurantModelAngular
+  //@Output() menus: any[] = [];
+  constructor(private http: HttpClient, private dashboardService: DashboardService, private router: Router, private route: ActivatedRoute) { 
+  // this.menuOutput = new IMenuModelAngular();
   }
+
+  // public generateMenuOutput(): any{
+  //   console.log(this.currentRestaurant.restaurantID);
+  //   this.dashboardService.getAllMenus(this.currentRestaurant.restaurantID).subscribe((res: any) => {
+  //     this.menuOutput = res;
+  //     console.log(`Menus: ${res}`); //${JSON.stringify(res)}
+  //   });
+  //   console.log(`Menus: ${this.menuOutput}`);
+  // }
   
   ngOnInit(): void {
-    console.log(this.currentRestaurant.restaurantID);
-    this.dashboardService.getAllMenus(this.currentRestaurant.restaurantID).subscribe((res: any) => {
-      this.menus = res;
-      console.log(`Menus: ${JSON.stringify(res)}`);
-    });
-    console.log(`Menus: ${this.menus}`);
-    //console.log(this.dashboardService.getAllMenus(this.currentRestaurant.restaurantID));
-  }
+    var resID="";
 
+    this.route.params.subscribe(curRes => {
+      resID = curRes['restaurantID'];
+
+    })
+
+    console.log(this.route.params);
+    this.dashboardService.getAllMenus(resID).subscribe((res: any) => {
+      var result = JSON.stringify(res);
+      this.menuOutput = JSON.parse(result);
+
+      console.log("result:"+result);
+      console.log("menuName"+this.menuOutput[0]['menuName']);
+     // this.menuOutput =res
+      //console.log(`Menus: ${JSON.stringify(res)}`); //${JSON.stringify(res)}
+    });
+    console.log(`MenusOut: ${this.menuOutput[0]}`);
+
+
+    // console.log(this.currentRestaurant.restaurantID);
+    // this.dashboardService.getAllMenus(this.currentRestaurant.restaurantID).subscribe((res: any) => {
+    //   this.menuOutput = res;
+    //   console.log(`Menus: ${res}`); //${JSON.stringify(res)}
+    // });
+    // console.log(`Menus: ${this.menuOutput}`);
+    //console.log(this.dashboardService.getAllMenus(this.currentRestaurant.restaurantID));
+
+    //generateRestaurant(): any {
+
+  }
 }
+
