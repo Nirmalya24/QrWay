b689030ba41147a4a56919e9ae07e2308058efa5
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-17 15:12:41 -0700

    Update routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RestaurantDashboardComponent } from './restaurant-dashboard/restaurant-dashboard.component';
-import { ManageMenuComponent } from './manageMenu/manage-menu/manage-menu.component';
+import { ManageMenuComponent } from './manage-menu/manage-menu.component';
 import { ViewRestaurantComponent } from './viewRestaurant/view-restaurant/view-restaurant.component';
 import { AppComponent } from './app.component';
-import { EditRestaurantComponent } from './editRestaurant/edit-restaurant/edit-restaurant.component';
+import { EditRestaurantComponent } from './edit-restaurant/edit-restaurant.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'dashboard', component: RestaurantDashboardComponent, pathMatch: 'full' },
   {path: 'menus', component: ManageMenuComponent},
-  {path:'editRes',component:EditRestaurantComponent}
+  {path:'restaurant',component:EditRestaurantComponent}
 ];
 
 @NgModule({
