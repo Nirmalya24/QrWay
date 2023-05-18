5bced0dbb2ecf2897d437343980ea4cf89dffae3
troy8chen <troy8chen@gmail.com>
2023-05-18 00:06:51 -0700

    update manage-menu.component.ts
    Update menu page variables

--- frontend/src/app/manage-menu/manage-menu.component.ts
+++ frontend/src/app/manage-menu/manage-menu.component.ts
@@ -10,24 +10,49 @@ import { JsonPipe } from '@angular/common';
 @Component({
   selector: 'app-managae-menu',
   templateUrl: './manage-menu.component.html',
-  styleUrls: ['./manage-menu.component.css'],
-  //template:'<p>{{ menuOutput }}</p>'
+  styleUrls: ['./manage-menu.component.css']
 })
 
 
-
 export class ManageMenuComponent {
-  //public menus: any[] = [];
-
-  //@Input() currentRestaurant: IRestaurantModelAngular;
   menuOutput:any[]=[];
-
-  //@Output() menus: any[] = [];
+  restaurantOutput: { restaurantName: string } = { restaurantName: '' };
+  
   constructor(private http: HttpClient, private dashboardService: DashboardService, private router: Router, private route: ActivatedRoute) { 
-  // this.menuOutput = new IMenuModelAngular();
   }
 
-  // public generateMenuOutput(): any{
+  ngOnInit(): void {
+    var resID="";
+
+    this.route.params.subscribe(curRes => {
+      resID = curRes['restaurantID'];
+      
+    })
+
+    console.log(this.route.params);
+    this.dashboardService.getAllMenus(resID).subscribe((res: any) => {
+      var result = JSON.stringify(res);
+      this.menuOutput = JSON.parse(result);
+    
+      //console.log("result:"+result);
+      //console.log("menuName"+this.menuOutput[0]['menuName']);
+      // this.menuOutput =res
+      //console.log(`Menus: ${JSON.stringify(res)}`); //${JSON.stringify(res)}
+    });
+    this.dashboardService.getRestaurant(resID).subscribe((res: any) => {
+      var result = JSON.stringify(res);
+      this.restaurantOutput = JSON.parse(result);
+    });
+    console.log(`MenusOut: ${this.menuOutput[0]}`);
+  }
+
+  getMenuSectionKeys(menu: IMenuModelAngular): string[] {
+    return Object.keys(menu.menuSections);
+  }  
+}
+
+
+// public generateMenuOutput(): any{
   //   console.log(this.currentRestaurant.restaurantID);
   //   this.dashboardService.getAllMenus(this.currentRestaurant.restaurantID).subscribe((res: any) => {
   //     this.menuOutput = res;
@@ -35,38 +60,3 @@ export class ManageMenuComponent {
   //   });
   //   console.log(`Menus: ${this.menuOutput}`);
   // }
-  
-  ngOnInit(): void {
-    var resID="";
-
-    this.route.params.subscribe(curRes => {
-      resID = curRes['restaurantID'];
-
-    })
-
-    console.log(this.route.params);
-    this.dashboardService.getAllMenus(resID).subscribe((res: any) => {
-      var result = JSON.stringify(res);
-      this.menuOutput = JSON.parse(result);
-
-      console.log("result:"+result);
-      console.log("menuName"+this.menuOutput[0]['menuName']);
-     // this.menuOutput =res
-      //console.log(`Menus: ${JSON.stringify(res)}`); //${JSON.stringify(res)}
-    });
-    console.log(`MenusOut: ${this.menuOutput[0]}`);
-
-
-    // console.log(this.currentRestaurant.restaurantID);
-    // this.dashboardService.getAllMenus(this.currentRestaurant.restaurantID).subscribe((res: any) => {
-    //   this.menuOutput = res;
-    //   console.log(`Menus: ${res}`); //${JSON.stringify(res)}
-    // });
-    // console.log(`Menus: ${this.menuOutput}`);
-    //console.log(this.dashboardService.getAllMenus(this.currentRestaurant.restaurantID));
-
-    //generateRestaurant(): any {
-
-  }
-}
-
