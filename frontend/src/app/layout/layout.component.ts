1c51abc7711bb3103d2e2a4e5be5f5a758d95968
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-17 23:47:44 -0700

    Added logic to show the correct button on different pages

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateRestaurantDialogComponent } from '../create-restaurant-dialog/create-restaurant-dialog.component';
-import { Router, NavigationEnd } from '@angular/router';
+import { CreateMenuDialogComponent } from '../create-menu-dialog/create-menu-dialog.component';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 @Component({
@@ -11,9 +12,11 @@ import { filter } from 'rxjs/operators';
 })
 export class LayoutComponent {
 
-  shouldShowCreateRestaurantButton = true;
+  shouldShowCreateRestaurantButton: boolean = true;
+  shouldShowCreateMenuButton: boolean;
 
-  constructor(public dialog: MatDialog, private router: Router) {
+  constructor(public dialog: MatDialog, private router: Router, private route: ActivatedRoute) {
+    this.shouldShowCreateMenuButton = this.checkDynamicURL();
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe(() => {
@@ -28,12 +31,30 @@ export class LayoutComponent {
 
   private evaluateConditions(): void {
     this.shouldShowCreateRestaurantButton = this.router.url === '/dashboard';
+    this.shouldShowCreateMenuButton = this.checkDynamicURL();
   }
 
-  openDialog() {
+  checkDynamicURL(): boolean {
+    const restaurantID = this.router.url.split('/')[2];
+    if(restaurantID && this.router.url.includes('menu')) {
+      return true;
+    }
+    return false;
+  }
+
+  openCreateRestaurantDialog() {
     this.dialog.open(CreateRestaurantDialogComponent, {
       data: {
         title: 'Create a new restaurant',
+      }
+    });
+  }
+
+  openCreateMenuDialog() {
+    this.dialog.open(CreateMenuDialogComponent, {
+      data: {
+        title: 'Create a new menu',
+        restaurantID: "testID"
       }
     });
   }
