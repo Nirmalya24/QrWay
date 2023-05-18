70038d52c28686cdd4fad75ac92c27082f404215
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-17 22:27:19 -0700

    Menu Card sets menu and redirects to menu mangement

--- frontend/src/app/menu-card/menu-card.component.ts
+++ frontend/src/app/menu-card/menu-card.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import IMenuModelAngular from '../share/IMenuModelAngular';
+import { MenuService } from '../services/restaurant/menu.service';
 
 @Component({
   selector: 'app-menu-card',
@@ -12,7 +13,7 @@ import IMenuModelAngular from '../share/IMenuModelAngular';
 export class MenuCardComponent {
   @Input() menu!: any;
 
-  constructor(private router: Router, private http: HttpClient) { 
+  constructor(private router: Router, private http: HttpClient, private menuService: MenuService) { 
     this.menu = {} as IMenuModelAngular;
   }
 
@@ -20,8 +21,9 @@ export class MenuCardComponent {
     console.log(`[MenuCardComponent] menu: ${JSON.stringify(this.menu)}`);
   }
 
-  public redirectToMenu(): any {
-    // this.router.navigate(['/menu/:restaurantID/:menuID', menuID]);
-    // console.log(`[MenuCardComponent] View button clicked: redirect to ${menuID}`);
+  public redirectToMenu(restaurantID: string, menuID: string): any {
+    this.menuService.setMenu(this.menu);
+    this.router.navigate(['/menus', restaurantID, menuID]);
+    console.log(`[MenuCardComponent] View button clicked: redirect to ${menuID}`);
   }
 }
