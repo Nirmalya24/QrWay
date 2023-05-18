ff53f0b258ba7fadf39fe43ab8b3ff4454dc3879
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-17 23:49:14 -0700

    Added getRestaurantName service function

--- frontend/src/app/services/restaurant/menu.service.ts
+++ frontend/src/app/services/restaurant/menu.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +16,13 @@ export class MenuService {
   // Fetch all menus from backend for a specific restaurant
   public getAllMenus(restaurantID: string): any {
     return this.http.get<any>(`${this.baseURL}/menus/${restaurantID}`);
+  }
+
+  
+
+  public getRestaurantName(restaurantID: string): Observable<string> {
+    return this.http.get<any>(`${this.baseURL}/restaurant/${restaurantID}`)
+      .pipe(map((restaurant: any) => restaurant.restaurantName));
   }
 
   setMenu(menu: any): void {
