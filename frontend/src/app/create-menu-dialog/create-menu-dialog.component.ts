7635084bae5d72e3c6c4045309ebdbd85092b088
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-18 02:57:31 -0700

    Create menu dialog works

--- frontend/src/app/create-menu-dialog/create-menu-dialog.component.ts
+++ frontend/src/app/create-menu-dialog/create-menu-dialog.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { MenuService } from '../services/restaurant/menu.service';
+import IMenuModelAngular from '../share/IMenuModelAngular';
 
 @Component({
   selector: 'app-create-menu-dialog',
@@ -10,19 +11,57 @@ import { MenuService } from '../services/restaurant/menu.service';
 })
 export class CreateMenuDialogComponent {
 
+  menuForm: FormGroup;
+  sendNewMenu: IMenuModelAngular;
+
   constructor(
     public dialogRef: MatDialogRef<CreateMenuDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private fb: FormBuilder,
     private menuService: MenuService) {
-
+      this.sendNewMenu = {} as IMenuModelAngular;
+      this.menuForm = this.fb.group({
+        menuName: ['', Validators.required],
+        menuDescription: [''],
+        menuSections: this.fb.array([]),
+        menuStartTime: ['', Validators.required],
+        menuEndTime: ['', Validators.required]
+      });
 
   }
 
   ngOnInit(): void {
+    console.log("[CreateMenuDialog] RestaurantID: ", this.data.restaurantID);
   }
 
-  onSubmit() {
+  get menuSections(): FormArray {
+    return this.menuForm.get('menuSections') as FormArray;
+  }
+
+  addMenuSection(): void {
+    this.menuSections.push(this.fb.control(''));
+  }
+
+  removeMenuSection(index: number): void {
+    this.menuSections.removeAt(index);
+  }
+
+  onSubmit(): void {
+    if (this.menuForm.valid) {
+      // Process the form data here
+      this.sendNewMenu.menuName = this.menuForm.value.menuName;
+      this.sendNewMenu.menuDescription = this.menuForm.value.menuDescription;
+      this.sendNewMenu.menuSections = this.menuForm.value.menuSections;
+      this.sendNewMenu.menuSections = [...this.menuForm.value.menuSections];
+      this.sendNewMenu.menuStartTime = this.menuForm.value.menuStartTime;
+      this.sendNewMenu.menuEndTime = this.menuForm.value.menuEndTime;
+      this.sendNewMenu.restaurantID = this.data.restaurantID;
+      // send form data to service
+      this.menuService.createMenu(this.sendNewMenu).subscribe((data: object) => {
+        console.log("[CreateMenuDialog] data: ", data);
+        this.dialogRef.close();
+      });
+    }
   }
 
 }
