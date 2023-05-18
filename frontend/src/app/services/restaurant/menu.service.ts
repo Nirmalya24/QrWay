f14f32c53b78e4cc8c7f140e089e3181cf0752d6
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-17 22:12:30 -0700

    WIP Menu Service

---
+++ frontend/src/app/services/restaurant/menu.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+
+  private baseURL: string = 'http://localhost:8000/api';
+  private menu: any = {};
+
+  constructor(private http: HttpClient) { }
+
+  // Fetch all menus from backend for a specific restaurant
+  public getAllMenus(restaurantID: string): any {
+    return this.http.get<any>(`${this.baseURL}/menus/${restaurantID}`);
+  }
+}
