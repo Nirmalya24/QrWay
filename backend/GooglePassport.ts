ed14c487c8d5eab21554ec065f1d56c71ca9aac2
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-28 12:30:27 -0700

    File formatting

--- backend/GooglePassport.ts
+++ backend/GooglePassport.ts
@@ -8,35 +8,36 @@ class GooglePassport {
 
     clientId: string;
     secretId: string;
-     
-    constructor() { 
+
+    constructor() {
         this.clientId = process.env.GOOGLE_OAUTH_ID;
         this.secretId = process.env.GOOGLE_OAUTH_SECRET;
 
         passport.use(new GoogleStrategy({
-                clientID: this.clientId,
-                clientSecret: this.secretId,
-                callbackURL: "/auth/google/callback"
-//                profileFields: ['id', 'displayName', 'emails']
-            },
+            clientID: this.clientId,
+            clientSecret: this.secretId,
+            callbackURL: "/auth/google/callback"
+            //                profileFields: ['id', 'displayName', 'emails']
+        },
             (accessToken, refreshToken, profile, done) => {
                 console.log("[GooglePassport] Inside new password google strategy");
-                process.nextTick( () => {
-                    console.log('[GooglePassport] validating google profile:' + JSON.stringify(profile));
-                    console.log("[GooglePassport] userId:" + profile.id);
-                    console.log("[GooglePassport] displayName: " + profile.displayName);
-                    console.log("[GooglePassport] Retrieved all of the profile info needed");
+                process.nextTick(() => {
+                    // console.log('[GooglePassport] validating google profile:' + JSON.stringify(profile));
+                    // console.log("[GooglePassport] userId:" + profile.id);
+                    // console.log("[GooglePassport] displayName: " + profile.displayName);
+                    // console.log("[GooglePassport] Retrieved all of the profile info needed");
+                    console.log(`[GooglePassport] \n\tid: ${profile.id}\n\tdisplayName: ${profile.displayName}\n\timage: ${profile.photos[0].value}\n\temail: ${profile.emails[0].value}`);
                     // this.email = profile.emails[0].value;
                     return done(null, profile);
-                }); 
+                });
             }
         ));
 
-        passport.serializeUser(function(user, done) {
+        passport.serializeUser(function (user, done) {
             done(null, user);
         });
 
-        passport.deserializeUser(function(user, done) {
+        passport.deserializeUser(function (user, done) {
             done(null, user);
         });
     }
