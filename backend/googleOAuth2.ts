18b799ff8b6d0a689af4286d1479aa0547e40ecc
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-05-30 18:09:11 -0700

    Delete unwanted file

--- backend/googleOAuth2.ts
+++
@@ -1,6 +0,0 @@
-class googleOAuth2 {
-    static id: string = process.env.GOOGLE_OAUTH_ID || '';
-    static secret: string = process.env.GOOGLE_OAUTH_SECRET || '';
-
-}
-export default googleOAuth2;
