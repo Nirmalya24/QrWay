9c110ecf3f5ef6f298456aae665edd149c43b413
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-06-02 15:25:11 -0700

    Update AppServer

--- backend/AppServer.ts
+++ backend/AppServer.ts
@@ -2,4 +2,6 @@ require('dotenv').config();
 import {App} from './App';
 
 let server: any = new App().expressApp;
-server.listen(8000);
+server.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
