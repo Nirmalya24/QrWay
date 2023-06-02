d37f64cbaae6c382de865f93d4534691c908c671
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-06-02 15:19:20 -0700

    Update App server to use env PORT value

--- backend/AppServer.ts
+++ backend/AppServer.ts
@@ -2,4 +2,6 @@ require('dotenv').config();
 import {App} from './App';
 
 let server: any = new App().expressApp;
-server.listen(8080);
+server.listen(process.env.PORT, () => {
+    console.log('Express server listening on port ' + process.env.PORT);
+});
