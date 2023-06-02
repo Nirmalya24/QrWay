f746c21ce3492e4d0a648b7c2e701c25f0517bc8
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-06-02 11:09:48 -0700

    test

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://qrway.azurewebsites.net/api'
+    // apiUrl: 'https://qrway.azurewebsites.net/api'
+    apiUrl: 'https://qrway.azurewebsites.net'
 };
