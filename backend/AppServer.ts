b6e84044e68621766877237d710348a8b6e9fa18
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-06-02 18:08:01 -0700

    test

--- backend/AppServer.ts
+++ backend/AppServer.ts
@@ -2,6 +2,6 @@ require('dotenv').config();
 import {App} from './App';
 
 let server: any = new App().expressApp;
-server.listen(process.env.PORT, () => {
-    console.log(`Server listening on port ${process.env.PORT}`);
+server.listen(process.env.PORT || 8080, () => {
+    console.log(`Server listening on port ${process.env.PORT || 8080}`);
 });
