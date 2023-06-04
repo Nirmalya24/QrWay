ad6a1a0f72a47aeb54febe4487953eaf56ef70c7
troy8chen <troy8chen@gmail.com>
2023-06-04 10:16:17 -0700

    Update multipleObjects.js
    replace the testing to get all menus

--- backend/test/multipleObjects.js
+++ backend/test/multipleObjects.js
@@ -1,90 +1,101 @@
+require("dotenv").config();
 var chai = require("chai");
 var chaiHttp = require("chai-http");
-var async = require("async");
-const port = process.env.PORT || 8080;
-const host = `http://localhost:${port}`;
-
 var expect = chai.expect;
-var should = chai.should();
+const apiURL = `http://localhost:${process.env.PORT}`;
 
-var http = require("http");
 chai.use(chaiHttp);
 
-describe("Test to get a LIST of restaurants", function () {
-  this.timeout(15000);
-
-  var requestResult;
-  var response;
+describe("Test to get all menus", function () {
+  const restaurantID = "b061d548-e85c-11ed-a05b-0242ac120003";
+  let responseBody;
+  let response;
 
   before(function (done) {
     chai
-      .request(host)
-      .get("/api/restaurant/all/d792c6be-e89c-11ed-a05b-0242ac120003")
-      .end(function (err, res) {
-        requestResult = res.body;
+      .request(apiURL)
+      .get(`/api/menus/${restaurantID}`)
+      .then(function (res) {
         response = res;
-        expect(err).to.be.null;
+        responseBody = res.body;
         done();
+      })
+      .catch(function (err) {
+        done(err);
       });
   });
 
-  it("Return status is 200", function (done) {
-    expect(response).to.have.status(200);
+  it("Return Status is 200 OK", function (done) {
+    expect(response.statusCode).to.equal(200);
     done();
   });
 
-  it("Response body is of type array", function (done) {
-    expect(response.body).to.be.an("array");
+  it("Returns value is an array", function (done) {
+    expect(responseBody).to.be.an("array");
     done();
   });
 
-  it("Each restaurant in array has expected attribute type", function (done) {
-    response.body.forEach((restaurant) => {
-      expect(restaurant).to.have.property("managerID").that.is.a("array");
-      expect(restaurant).to.have.property("menusID").that.is.a("array");
-      expect(restaurant).to.have.property("restaurantName").that.is.a("string");
-      expect(restaurant).to.have.property("restaurantID").that.is.a("string");
-      expect(restaurant)
-        .to.have.property("restaurantOwnerID")
-        .that.is.a("string");
-      expect(restaurant).to.have.property("description").that.is.a("string");
-      expect(restaurant)
-        .to.have.property("restaurantImage")
-        .that.is.a("string");
-      expect(restaurant).to.have.property("tag").that.is.a("string");
-    });
+  it("Return type of body is JSON", function (done) {
+    expect(response).to.be.json;
     done();
   });
 
-  it("Each restaurant in array has expected attribute name", function (done) {
-    response.body.forEach((restaurant) => {
+  it("Restaurants contains expected attribute names", function (done) {
+    responseBody.forEach(function (restaurant) {
       expect(restaurant).to.have.all.keys(
-        "managerID",
-        "menusID",
         "_id",
-        "restaurantName",
+        "menuID",
         "restaurantID",
-        "restaurantOwnerID",
-        "description",
-        "restaurantImage",
-        "tag"
+        "menuName",
+        "menuItems",
+        "menuSections",
+        "menuDescription",
+        "menuStartTime",
+        "menuEndTime",
+        "public"
       );
     });
     done();
   });
 
-  it("Each restaurant in array has a MongoDB ID", function (done) {
-    response.body.forEach((restaurant) => {
-      expect(restaurant).to.have.property("_id");
+  it("Item contains expected attribute types", function (done) {
+    const expectedAttributeTypes = {
+      "_id": "string",
+      "menuID": "string",
+      "restaurantID": "string",
+      "menuName": "string",
+      "menuItems": "array",
+      "menuSections": "object",
+      "menuDescription": "string",
+      "menuStartTime": "string",
+      "menuEndTime": "string",
+      "public": "boolean"
+    }
+  
+    responseBody.forEach(function (item) {
+      Object.keys(expectedAttributeTypes).forEach(function (key) {
+        const expectedType = expectedAttributeTypes[key];
+        expect(item[key]).to.be.a(expectedType);
+      });
+    });
+  
+    done();
+  });
+  
+
+  it("Item contains _id", function (done) {
+    responseBody.forEach(function (item) {
+      expect(item).to.have.property("_id");
     });
     done();
   });
-
-  it("Each restaurant in array has a Restaurant ID", function (done) {
-    response.body.forEach((restaurant) => {
-      expect(restaurant).to.have.property("restaurantID");
+  
+  it("Item contains restaurantID", function (done) {
+    responseBody.forEach(function (item) {
+      expect(item).to.have.property("restaurantID");
     });
-
     done();
   });
+  
+
 });
