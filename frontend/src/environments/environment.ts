aedce753572962d90ff94abf894177c5d4637ff4
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2023-06-05 22:09:53 -0700

    Update angular environments

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://qrway.azurewebsites.net/api'
-    apiUrl: 'http://localhost:8080/api'
-
+    apiUrl: 'https://qrway.azurewebsites.net/api'
 };
